7df512168176a78bfda844229315b89d86b40fd2
Jean-Luc Martin <jean-luc.martin@kandy.io>
2016-03-09 01:17:58 -0500

    added counter

---
+++ src/increment.js
@@ -0,0 +1,5 @@
+function increment(number) {
+    return number + 2;
+}
+
+export default increment;
--- src/root.js
+++ src/root.js
@@ -1,8 +1,26 @@
+import increment from './increment';
+
 const Root = {
-    render() {
+    initialState() {
+        return {
+            count: 0
+        };
+    },
+
+    render({state}, setState) {
+        function incrementCount() {
+            setState({count: increment(state.count)});
+        }
+
         return (
             <div>
-                This is the Deku-Boilerplate
+                <div>
+                    This is the Deku-Boilerplate
+                </div>
+                <div>
+                    Current Count: {state.count}
+                </div>
+                <button onClick={incrementCount}>Increment</button>
             </div>
         );
     }
